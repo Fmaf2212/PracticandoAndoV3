87bf7c4fc0e7033935bcd047179cbfb1520a4279
moises <fmaf_3011@outlook.com>
2020-12-17 20:05:15 -0500

    add function on click

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+//BEGIN VIDEOS DE LOS SOCIOS DESTACADOS
+$(document).ready(function () {
+
+    $('.btn-vertical-slider').on('click', function () {
+
+        if ($(this).attr('data-slide') == 'next') {
+            $('#myCarousel').carousel('next');
+        }
+        if ($(this).attr('data-slide') == 'prev') {
+            $('#myCarousel').carousel('prev')
+        }
+
+    });
+});
